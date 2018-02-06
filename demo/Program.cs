605a3ebd6ec8d0b4e74636e901dd2ef53609bc38
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-02-06 18:51:07 +0700

    a

--- demo/Program.cs
+++ demo/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using Renci.SshNet.Async;
 using System.IO;
+using System.Threading;
 
 namespace demo
 {
@@ -14,12 +15,13 @@ namespace demo
         {
             // initialize client and connect like you normally would
             //var client = new SshClient("127.0.0.1", 55555, "admin", " ");
-            var client = new SshClient("127.0.0.1", 55555, "admin", " ");
-            client.Connect();
+            //var client = new SshClient("127.0.0.1", 55555, "admin", " ");
+            //client.Connect();
 
             // quick way to use ist, but not best practice - SshCommand is not Disposed, ExitStatus not checked...
-            Console.WriteLine(client.CreateCommand("ls -l").Execute());
+            //Console.WriteLine(client.CreateCommand("ls -l").Execute());
 
+            //Console.WriteLine(client.CreateCommand("gcc -v").Execute());
 
             //// await a directory listing
             //var listing = await client.ListDirectoryAsync(".");
@@ -31,8 +33,42 @@ namespace demo
             //}
 
             // disconnect like you normally would
-            client.Disconnect();
+            //client.Disconnect();
+
+            ExecuteCommandOnThreadPool();
+
             Console.ReadLine();
         }
+         
+
+        public static void ExecuteCommandOnThreadPool()
+        {
+            string host = "127.0.0.1";
+            string user = "admin";
+            string pass = " ";
+
+            Action runCommand = () =>
+            {
+                SshClient client = new SshClient(host, 55555, user, pass);
+                try
+                {
+                    client.Connect();
+                    //var terminal = client.RunCommand("/bin/run.sh");
+
+                    Thread.Sleep(3000);
+
+                    var terminal = client.RunCommand("gcc -v");
+
+                    Console.WriteLine(terminal.Result);
+                }
+                finally
+                {
+                    client.Disconnect();
+                    client.Dispose();
+                }
+            };
+
+            ThreadPool.QueueUserWorkItem(x => runCommand());
+        }
     }
-}
+} 
